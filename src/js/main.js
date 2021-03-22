10d2d0a3c7774b57bb14d45107d707abdf8e149c
Marina Pochivalova <marina.tokareva85@gmail.com>
2021-03-22 14:25:18 +0700

    carousel buttons, links of top/footer menu

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//example from https://gnatkovsky.com.ua/yakorya-i-plavnyj-perexod-po-yakornym-ssylkam.html
+function scrollTo(id_navigation){
+    $(id_navigation).on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;                  
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+}
+
+$(document).ready(function(){
+    scrollTo("#navigatin-top");
+    scrollTo("#navigation-footer");    
+});
