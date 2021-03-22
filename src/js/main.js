cfff719b3884dcbe9333144c2bd8d38bda0f3f22
Marina Pochivalova <marina.tokareva85@gmail.com>
2021-03-23 00:28:09 +0700

    add slick slider

--- src/js/main.js
+++ src/js/main.js
@@ -10,5 +10,43 @@ function scrollTo(id_navigation){
 
 $(document).ready(function(){
     scrollTo("#navigation-top");
-    scrollTo("#navigation-footer");    
+    scrollTo("#navigation-footer");      
+});
+
+$(document).ready(function(){
+$('.carousel').slick({    
+    slidesToShow:4,
+    slidesToScroll: 1,   
+    speed:200,
+    prevArrow:document.getElementById("slick-prev"),
+    nextArrow:document.getElementById("slick-next"),
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 });
